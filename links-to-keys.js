0a0ff3cf89d06aa1f99e772cb2e4bb3cf85317bc
glutaminefree <glutaminefree@mail.ru>
2014-09-22 16:45:15 +0400

    Force render buttons feature, bug fixes

--- links-to-keys.js
+++ links-to-keys.js
@@ -1,17 +1,18 @@
 // ==UserScript==
 // @name		Links To Keys
 // @namespace	linkstokeys
-// @version		0.03.1
+// @version		0.04
 // @grant		none
 // ==/UserScript==
 
 document.addEventListener('DOMContentLoaded', function(){
 	var nums	= [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-	var buttons = [];
 	var input	= false;
 	var shift	= false;
 	var ready	= false;
 	var buffer	= '';
+	var force;
+	var buttons;
 	var button;
 	var links;
 	var link;
@@ -19,52 +20,55 @@ document.addEventListener('DOMContentLoaded', function(){
 	var i;
 
 	window.addEventListener('keypress', function(event){
-		var activeNode = document.activeElement;
-
-		if (
-			( activeNode.nodeName.toLowerCase() == 'input' ) ||
-			( activeNode.nodeName.toLowerCase() == 'textarea' ) ||
-			( activeNode.hasAttribute('contenteditable') && activeNode.getAttribute('contenteditable') == 'true' )
-			) {
-			return true;
-		}
-
 		if ( ( event.key.toLowerCase() == 'f' ) && !event.ctrlKey ) {
-			// Build helpers
-			links = document.querySelectorAll('a');
+			force = event.altKey;
 
-			for(i = 0; i < links.length; i++) {
-				// Create new element
-				button = document.createElement('span');
-				button.appendChild( document.createTextNode(i) );
+			if ( !force ) {
+				var activeNode = document.activeElement;
 
-				button.className = 'links-to-keys-button';
-
-				// Set styles to element
-				button.style.display			= 'none';
-				button.style.position			= 'absolute';
-				button.style.padding			= '2px';
-				button.style.backgroundColor	= 'yellow';
-				button.style.fontSize			= '10px';
-				button.style.color				= 'red';
-
-				links[i].className += ' linkstokeys-'+i;
-
-				links[i].parentNode.insertBefore( button, links[i] );
-
-				buttons.push( button );
-			}
-
-			for(i = 0; i < buttons.length; i++) {
-				if ( buttons[i].style.display == 'none' ) {
-					buttons[i].style.display = 'inline';
-					ready = true;
-				} else {
-					buttons[i].style.display = 'none';
-					ready = false;
+				if (
+					( activeNode.nodeName.toLowerCase() == 'input' ) ||
+					( activeNode.nodeName.toLowerCase() == 'textarea' ) ||
+					( activeNode.hasAttribute('contenteditable') && activeNode.getAttribute('contenteditable').toString() == 'true' )
+					) {
+					return true;
 				}
 			}
 
+			if ( !ready ) {
+				// Build helpers
+				links	= document.querySelectorAll('a');
+				buttons	= [];
+
+				for(i = 0; i < links.length; i++) {
+					// Create new element
+					button = document.createElement('span');
+					button.appendChild( document.createTextNode(i) );
+
+					button.className = 'links-to-keys-button';
+
+					// Set styles to element
+					button.style.display			= 'inline';
+					button.style.position			= 'absolute';
+					button.style.padding			= '2px';
+					button.style.backgroundColor	= 'yellow';
+					button.style.fontSize			= '10px';
+					button.style.color				= 'red';
+
+					links[i].className += ' linkstokeys-'+i;
+
+					links[i].parentNode.insertBefore( button, links[i] );
+
+					buttons.push( button );
+				}
+			} else {
+				for(i = 0; i < buttons.length; i++) {
+					links[i].className = links[i].className.replace( ' linkstokeys-' + i, '' );
+					buttons[i].parentNode.removeChild(buttons[i]);
+				}
+			}
+
+			ready = !ready;
 			shift = event.shiftKey;
 
 			event.preventDefault();
@@ -81,16 +85,17 @@ document.addEventListener('DOMContentLoaded', function(){
 							var a = document.querySelector( 'a.linkstokeys-' + buffer );
 
 							if ( a ) {
-								if ( shift ) {
+								if ( shift )
 									a.setAttribute('target', '_blank');
 
-									for(i = 0; i < buttons.length; i++)
-										buttons[i].style.display = 'none';
+								a.click();
 
-									ready = false;
+								for(i = 0; i < buttons.length; i++) {
+									links[i].className = links[i].className.replace( ' linkstokeys-' + i, '' );
+									buttons[i].parentNode.removeChild(buttons[i]);
 								}
 
-								a.click();
+								ready = false;
 							}
 
 							buffer = '';
